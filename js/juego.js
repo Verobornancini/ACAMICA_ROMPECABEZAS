6ad6639ed802e165cd20f1ee740ac821bced5a51
Vero ^^ <verobornancini@gmail.com>
2019-06-04 12:37:48 -0300

    Juego completo

--- js/juego.js
+++ js/juego.js
@@ -11,10 +11,22 @@ var grilla = [
     [7, 8, 9]
 ];
 
+//guardo la formacion de la grilla ganadora en una variable
+var grillaGanadora = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
 /* Estas dos variables son para guardar la posición de la pieza vacía. 
 Esta posición comienza siendo la [2, 2]*/
 var filaVacia = 2;
 var columnaVacia = 2;
+
+/*Al iniciar el juego se mezclan las fichas y en ciertas ocaciones durante la mezcla aleatoria pasan por la posicion correcta de la
+grillaGanadora, por lo que se ejecuta la funcion mostrarCartelGanador(), para evitar que se ejecute esta funcion antes de que el
+jugador mueva al menos una ficha cree esta variable booleana o flag*/
+var puedeMostrarCartelGanador=0;
 
 /* Esta función deberá recorrer el arreglo de instrucciones pasado por parámetro. 
 Cada elemento de este arreglo deberá ser mostrado en la lista con id 'lista-instrucciones'. 
@@ -35,23 +47,19 @@ function guardarUltimoMomiviento(direccion) {
 /* Esta función va a chequear si el Rompecabezas esta en la posicion ganadora. 
 Existen diferentes formas de hacer este chequeo a partir de la grilla. */
 function chequearSiGano() {
-  for (var i=0; i< grilla.length;i++){
-    for (var j=0; j< grilla.length;j++){
-        if (grilla==[[1, 2, 3],[4, 5, 6],[7, 8, 9]]){
-          return true;
-        } 
-        else {
-          return false;
-        }
+  for (var i=0; i<grilla.length;i++) {
+    for (var j=0; j<grilla.length;j++) {
+     if (grilla[i][j]!==grillaGanadora[i][j]){
+        return false;
+      } 
     }
   }
+  mostrarCartelGanador();
 }
 
 // Implementar alguna forma de mostrar un cartel que avise que ganaste el juego
 function mostrarCartelGanador() {
-    if (chequearSiGano==true){
-      alert ('Has ganado la partida');
-    }
+      swal('Felicitaciones ! Has ganado la partida!');
 }
 
 /* Función que intercambia dos posiciones en la grilla.
@@ -70,12 +78,11 @@ function actualizarPosicionVacia(nuevaFila, nuevaColumna) {
 
 // Para chequear si la posicón está dentro de la grilla.
 function posicionValida(fila, columna) {
-    if ((fila>0 && fila<=grilla[i].length) && (columna>0 && columna<=grilla[j].length)){
-      return true;
-    }
-    else{
-      return false;
-    }
+      if ((fila>=0 && fila<grilla.length) && (columna>=0 && columna<grilla[fila].length)){
+        return true;
+      } else{
+        return false;
+  }
 }
 
 /* Movimiento de fichas, en este caso la que se mueve es la blanca intercambiando su posición con otro elemento.
@@ -99,13 +106,13 @@ function moverEnDireccion(direccion) {
   // Mueve pieza hacia la derecha, reemplazandola con la blanca
   else if (direccion === codigosDireccion.DERECHA) {
     nuevaFilaPiezaVacia = filaVacia;
-    nuevaColumnaPiezaVacia = columnaVacia + 1;
+    nuevaColumnaPiezaVacia = columnaVacia - 1;
   }
     
   // Mueve pieza hacia la izquierda, reemplazandola con la blanca
   else if (direccion === codigosDireccion.IZQUIERDA) {
     nuevaFilaPiezaVacia = filaVacia;
-    nuevaColumnaPiezaVacia = columnaVacia - 1;
+    nuevaColumnaPiezaVacia = columnaVacia + 1;
   }
 
   /* A continuación se chequea si la nueva posición es válida, si lo es, se intercambia. 
@@ -116,6 +123,7 @@ function moverEnDireccion(direccion) {
         intercambiarPosiciones(filaVacia, columnaVacia, nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia);
         actualizarPosicionVacia(nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia);
         guardarUltimoMomiviento(direccion);
+        chequearSiGano();
     }
 }
 
